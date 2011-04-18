3967529ea91ac98f4d41838eb312be97506b3188
Yigit Ozkan <yigitozkan2804@gmail.com>
2011-04-19 00:53:51 +0300

    added apache license

--- bash/tor_script.sh
+++ bash/tor_script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+########################################################
+# Copyright 2011 Yigit Ozkan.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+########################################################
+
 echo "deb http://deb.torproject.org/torproject.org experimental-lucid main" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 886DDD89
 sudo apt-get update 
