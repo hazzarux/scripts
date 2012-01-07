fe71bda47967c3177d1018f4e1aa144833de5b60
Yigit Ozkan <yigitozkan2804@gmail.com>
2012-01-08 00:08:28 +0100

    britney stuff :)

---
+++ python/britney/britney_spears.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+from BeautifulSoup import BeautifulSoup
+import urllib
+from pprint import pprint
+
+target = 'http://britneyspears.ac/lyrics.htm'
+target = urllib.urlopen(target)
+target = target.read()
+soup = BeautifulSoup(target)
+result_file = open("britney_spears_results.txt",'w')
+
+for elem in soup.findAll('h5','lyrics'):
+  links = elem.findAll('a')
+  for link in links:
+    contents = link.contents
+    for content in contents:
+      content = content.split()
+      content.pop(0)
+      content = ' '.join(content)
+      if content[0].capitalize() =='I':
+        result_file.write(content)
+        result_file.write('\n')
+
