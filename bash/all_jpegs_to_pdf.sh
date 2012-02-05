65996df3a202f5281a27420c4dfc02e51b667e30
Yigit Ozkan <yigitozkan2804@gmail.com>
2012-02-05 12:49:44 +0100

    updated script

--- bash/all_jpegs_to_pdf.sh
+++ bash/all_jpegs_to_pdf.sh
@@ -9,15 +9,22 @@
 
 
 # converts all jpeg images in a directory to 1 single pdf file
+# after that it 'prints' the pdf file into another one
+# 2 virtual pages per physical page (2-up)
 # uses imagemagick
 
-sudo apt-get install imagemagick
+if ! which imagemagick >/dev/null; then
+    echo "imagemagick is installed"
+else
+    sudo apt-get install imagemagick
+    echo "imagemagick has been installed"
+fi
 
 fname="${PWD##*/}"
 fname="${fname}.pdf"
 
-echo "Starting..."
+echo "Creating first pdf..."
 convert -density 300 *.jpg "$fname"
+echo "Creating 2-up'ed pdf..."
+pdfnup "$fname" --nup 2x1 --outfile "$fname"
 echo "All done, captain!"
-
-#todo: 2-up the pdf: pdfnup complexe_getallen.pdf --nup 2x1 --outfile out.pdf
