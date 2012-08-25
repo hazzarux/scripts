fbc0ded86eb200e12678968e91fee14f803e4865
Yigit Ozkan <yigitozkan2804@gmail.com>
2012-08-25 14:22:11 +0200

    added host variable

--- python/mysql_automator/mysqldump_automator.py
+++ python/mysql_automator/mysqldump_automator.py
@@ -3,13 +3,15 @@
 
 from subprocess import Popen, PIPE
 
-DBNAME = 'cdcol'
+HOST = "localhost"
 USERNAME = 'root'
+DBNAME = 'cdcol'
 DEST_FOLDER = r'C:\Users\xoxo\Desktop\db_backup/'
+
 
 SQLNAME = DEST_FOLDER+DBNAME+".sql"
 
-command = 'mysqldump.exe -u {USERNAME} {DBNAME} > {SQLNAME}'.format(**locals())
+command = 'mysqldump.exe --host={HOST} -u {USERNAME} {DBNAME} > {SQLNAME}'.format(**locals())
     
 print "Running command.."
 
