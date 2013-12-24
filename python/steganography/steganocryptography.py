b9f454c91e67815c432f2e2806e4f4f23337038f
Yigit Ozkan <yigitozkan2804@gmail.com>
2013-12-24 20:31:39 +0100

    refactoring

--- python/steganography/steganocryptography.py
+++ python/steganography/steganocryptography.py
@@ -14,28 +14,27 @@ X = "moon.jpg"
 Y = "steganomoon.png" #stepic works better with png files!
 
 
-
-def create_encoded_image(input,message,output):
-    '''
+def create_encoded_image(input, message, output):
+    """
     creates an encoded image (output file)
-    '''
+    """
     i = Image.open(input)
-    i2 = stepic.encode(i,message)
+    i2 = stepic.encode(i, message)
     i2.save(output),
 
+
 def decode_message(file):
-    '''
+    """
     returns the message in the file
-    '''
+    """
     i = Image.open(file)
     data = stepic.decode(i)
     msg = data.decode()
     return msg
 
 
-create_encoded_image(X,SECRET_MSG,Y)
-data=decode_message(Y)
-
+create_encoded_image(X, SECRET_MSG, Y)
+data = decode_message(Y)
 
 if data == SECRET_MSG:
     print "[+] match"
