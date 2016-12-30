d8b173b966fb6b16eb114ebe1b536de3e12bd24e
yigit o <yigitozkan2804@gmail.com>
2016-12-30 18:55:29 +0100

    try-except because unicode was failing added extensions to gitignore

--- python/csv_operations/csv_op.py
+++ python/csv_operations/csv_op.py
@@ -10,14 +10,18 @@ copy&paste this into mailing solution
 import csv
 import codecs
 
-csvReader = csv.reader(codecs.open("google.csv",'rU',"utf16"))
-csvWriter = csv.writer(codecs.open("result13.csv",'wb',"utf16"),delimiter=',')
+csvReader = csv.reader(codecs.open("google.csv",'rU',"utf-16"))
+csvWriter = csv.writer(codecs.open("result13.csv",'wb',"utf-16"),delimiter=',')
 
 for row in csvReader:
-    name=row[0]
-    email=row[28]
-    name=name.replace(",","")
-    print("name",name)
-    print("email",email)
-    csvWriter.writerow([name,email])
-    print("[+] csv written")
+    try:
+        name=row[0]
+        email=row[28]
+        name=name.replace(",","")
+        print("name",name)
+        print("email",email)
+        csvWriter.writerow([name,email])
+        print("[+] csv written")
+    except:
+        pass
+    
